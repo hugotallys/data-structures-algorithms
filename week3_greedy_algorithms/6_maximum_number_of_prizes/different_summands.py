011c72791fb9c1fae2d54fdf2b1d8835ebf8aede
htmo.ufal <htmo.ufal@jfal.jus.br>
2025-12-04 20:42:08 -0300

    feat: Add greedy problem 6 solution

--- week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
+++ week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
@@ -1,11 +1,26 @@
+import math
+
+
 def optimal_summands(n):
-    summands = []
-    # write your code here
-    return summands
+
+    sizes = []
+
+    k = int(math.sqrt(2*n))
+
+    for i in range(1, k + 1):
+        remaining = n - i
+        if remaining > i:
+            sizes.append(i)
+            n = remaining
+        else:
+            sizes.append(i + remaining)
+            return sizes
+
+    return sizes
 
 
-if __name__ == '__main__':
-    n = int(input())
-    summands = optimal_summands(n)
+if __name__ == "__main__":
+    n_input = int(input())
+    summands = optimal_summands(n_input)
     print(len(summands))
     print(*summands)
