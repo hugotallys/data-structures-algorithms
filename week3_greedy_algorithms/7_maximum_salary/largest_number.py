3557088a1e86cfb2f704a4640e3c57284897c2a7
htmo.ufal <htmo.ufal@jfal.jus.br>
2025-12-04 22:22:26 -0300

    feat add solution greedy problem 7

--- week3_greedy_algorithms/7_maximum_salary/largest_number.py
+++ week3_greedy_algorithms/7_maximum_salary/largest_number.py
@@ -1,18 +1,22 @@
-from itertools import permutations
+from functools import cmp_to_key
+
+
+def compare_concatenate(a, b):
+    ab = int(str(a) + str(b))
+    if ab > (int(str(b) + str(a))):
+        return -1
+    return 1
 
 
 def largest_number_naive(numbers):
     numbers = list(map(str, numbers))
 
-    largest = 0
+    numbers = sorted(numbers, key=cmp_to_key(compare_concatenate))
 
-    for permutation in permutations(numbers):
-        largest = max(largest, int("".join(permutation)))
-
-    return largest
+    return int("".join(numbers))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     _ = int(input())
     input_numbers = input().split()
     print(largest_number_naive(input_numbers))
