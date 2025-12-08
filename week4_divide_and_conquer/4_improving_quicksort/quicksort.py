d371bbe0544bbf268c88c8fd2609b60ead44e1a6
htmo.ufal <htmo.ufal@jfal.jus.br>
2025-12-08 17:18:33 -0300

    Implements 3 partition quicksort

--- week4_divide_and_conquer/4_improving_quicksort/quicksort.py
+++ week4_divide_and_conquer/4_improving_quicksort/quicksort.py
@@ -8,33 +8,38 @@ def swap(arr, i, j):
     arr[i] = arr[j]
     arr[j] = tmp
 
-def partition(arr, p, r):
+def partition3(arr, p, r):
     """
-    Rearranges arr[p...r] returning an index q such that:
-    p   <= k <= q-1 -> arr[k] <= arr[r]
-    q+1 <= k <= r   -> arr[k] >  arr[r]
-    q   == k        -> arr[k] == arr[r]
+    Rearranges arr[p...r] returning indexes q, s such that:
+    p   <= k <= q-1 -> arr[k] < arr[r]
+    s+1 <= k <= r   -> arr[k] >  arr[r]
+    q   <= k <= s   -> arr[k] == arr[r]
     """
     x = arr[r]
     i = p - 1
+    k = p - 1
     for j in range(p, r):
-        if arr[j] <= x:
+        if arr[j] < x:
             i = i + 1
             swap(arr, i, j)
-    swap(arr, i + 1, r)
-    return i + 1
+        if arr[j] == x:
+            k = max(i, k) + 1
+            swap(arr, k, j)
+    t = max(i, k)
+    swap(arr, t + 1, r)
+    return i + 1, t + 1
 
 def quicksort(arr, p, r):
     """
     Sort the array inplace in O(n*n) (worst scenario).
     """
     if p < r:
-        q = partition(arr, p, r)
+        q, s = partition3(arr, p, r)
         quicksort(arr, p, q-1)
-        quicksort(arr, q+1, r)
+        quicksort(arr, s+1, r)
 
 if __name__ == "__main__":
-    numbers = [2, 8, 7, 1, 3, 5, 6, 4]
+    numbers = [13, 19, 9, 5, 12, 8, 7, 4, 21, 2, 6, 11, 11, 11, 11, 11, 11, 11]
     print(f"Numbers = {numbers}")
     quicksort(numbers, 0, len(numbers)-1)
     print(f"Sorted Numbers = {numbers}")
--- week4_divide_and_conquer/4_improving_quicksort/sorting.py
+++ week4_divide_and_conquer/4_improving_quicksort/sorting.py
@@ -1,9 +1,25 @@
 from random import randint
 
+def swap(arr, i, j):
+    """Swaps arr[i] and arr[j] inplace."""
+    tmp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = tmp
 
 def partition3(array, left, right):
-    # write your code here
-
+    x = array[right]
+    i = left - 1
+    k = left - 1
+    for j in range(left, right):
+        if array[j] < x:
+            i = i + 1
+            swap(array, i, j)
+        if array[j] == x:
+            k = max(i, k) + 1
+            swap(array, k, j)
+    t = max(i, k)
+    swap(array, t + 1, right)
+    return i + 1, t + 1
 
 def randomized_quick_sort(array, left, right):
     if left >= right:
