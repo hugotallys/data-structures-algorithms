72ffb66b9cf7f2b2945ba0824d90efae04618f43
htmo.ufal <htmo.ufal@jfal.jus.br>
2025-12-08 18:35:25 -0300

    add change problem solution (DP)

--- week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -1,9 +1,17 @@
-def change(money):
-    # write your code here
-
-    return money
+import math
 
 
-if __name__ == '__main__':
+def change(money, coins):
+    minimum_coins = [0 for _ in range(money + 1)]
+    for m in range(1, money + 1):
+        minimum_coins[m] = math.inf
+        for i, coin_i in enumerate(coins):
+            if m >= coins[i]:
+                if minimum_coins[m - coins[i]] + 1 < minimum_coins[m]:
+                    minimum_coins[m] = minimum_coins[m - coins[i]] + 1
+    return minimum_coins[money]
+
+
+if __name__ == "__main__":
     m = int(input())
-    print(change(m))
+    print(change(m, [1, 8, 20]))
